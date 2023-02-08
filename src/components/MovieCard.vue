b8aab8b939cf012495a11ab4e6c3682d21d39963
giuseppe_bisconti <giuseppebisconti00@gmail.com>
2023-02-08 16:06:05 +0100

    inizializzazione progetto

---
+++ src/components/MovieCard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
